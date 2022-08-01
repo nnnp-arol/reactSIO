ba77c6d96579f5470b71b6bb471d3fca1f66a1b3
nnnp-arol <arol.alonso@nonerds.com>
2022-08-01 01:15:08 -0300

    1er commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+
+import {PORT} from './config.js'
+
+const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+    }
+})
+
+app.use(cors())
+app.use(morgan('dev'))
+
+io.on('connection', (socket) => {
+    console.log('user connected ', socket.id)
+})
+
+server.listen(PORT)
+console.log('Server on port ', PORT)
