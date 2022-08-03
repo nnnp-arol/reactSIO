607f1d2b72cb5d1c20a84384760f35ce15c64d0a
arol.alonso <arol.alonso@nonerds.com>
2022-08-03 03:12:36 -0300

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,59 @@
-import './App.css';
-import io from 'socket.io-client'
-import {useState, useEffect} from 'react'
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
 
-
-const socket = io('http://localhost:4000')
+const socket = io("http://localhost:4000");
 
 function App() {
-
-  const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState([{
-    body: "message test",
-    from: "user1"
-  }])
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(message)
-    socket.emit('message', message)
+    e.preventDefault();
+    socket.emit("message", message);
     const newMessage = {
       body: message,
-      from: "me"
-    }
-    setMessages([newMessage, ...messages])
-    setMessage('')
-  }
+      from: "Me",
+    };
+    setMessages([newMessage, ...messages]);
+    setMessage("");
+  };
 
   useEffect(() => {
-    const receiveMessage = message => {
-      setMessages([message, ...messages])
-    }
-    socket.on('message', receiveMessage)
+    const receiveMessage = (message) => {
+      setMessages([message, ...messages]);
+    };
+    socket.on("message", receiveMessage);
 
     return () => {
-    socket.off('message', receiveMessage)
-
-    }
-  }, [messages])
+      socket.off("message", receiveMessage);
+    };
+  }, [messages]);
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setMessage(e.target.value)} value={message}/>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
         <button>send</button>
       </form>
-
-      {
-        messages.map((message, index) => {
-          <div key={index}>
-            <p>{message.from}: {message.body}</p>
-          </div>
-        })
-      }
+      <div className="body-msg">
+        {messages.map((message, index) => {
+          return (
+            <div
+              key={index}
+              className={message.from === "Me" ? "ownself" : "alien"}
+            >
+              <p>
+                {message.from}: {message.body}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,29 +4,29 @@ import { Server as SocketServer } from "socket.io";
 import http from "http";
 import cors from "cors";
 
-import {PORT} from './config.js'
+import { PORT } from "./config.js";
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 const io = new SocketServer(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-    }
-})
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
 
-app.use(cors())
-app.use(morgan('dev'))
+app.use(cors());
+app.use(morgan("dev"));
 
-io.on('connection', (socket) => {
-    console.log('user connected ', socket.id)
+io.on("connection", (socket) => {
+  console.log("user connected ", socket.id);
 
-    socket.on('message', (message) => {
-        socket.broadcast.emit('message', {
-            body: message,
-            from: socket.id
-        })
-    })
-})
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
 
-server.listen(PORT)
-console.log('Server on port ', PORT)
+server.listen(PORT);
+console.log("Server on port ", PORT);
